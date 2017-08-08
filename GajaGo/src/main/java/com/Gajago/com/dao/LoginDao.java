4ad6215b5634998f58c63f219e21e9d53120987c
tigggi <tigggi@DESKTOP-BELBUD5>
2017-08-09 01:02:33 +0900

    ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ

---
+++ GajaGo/src/main/java/com/Gajago/com/dao/LoginDao.java
@@ -0,0 +1,12 @@
+package com.Gajago.com.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class LoginDao {
+	@Autowired
+	private SqlSession sqlsession;
+
+}
